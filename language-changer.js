2c239dfb32e62c49bef8989d2e31a264e0d039d1
Rafał Araszkiewicz <rafal.araszkiewicz@setapp.pl>
2018-06-13 11:52:42 +0200

    Changes extension structure and adds redirection

---
+++ language-changer.js
@@ -0,0 +1,24 @@
+function languageReplacer(match, p1, p2, p3, offset, string) {
+    return [p1, 'en-us', p3].join('');
+  }
+
+function changeDocsLanguage(requestDetails) {
+    let modifiedUrl = requestDetails.url.replace(/(^https?:\/\/docs\.microsoft\.com\/)(\w+-?\w+)([\/*\w*\/*\W*]*)/, languageReplacer);
+
+    if(requestDetails.url === modifiedUrl){
+      return;
+    }
+
+    return {
+      redirectUrl: modifiedUrl
+    };
+  }
+  
+  browser.webRequest.onBeforeRequest.addListener(
+    changeDocsLanguage,
+    {
+      urls: ["*://docs.microsoft.com/*-*/*"],
+      types: ["main_frame"]
+    },
+    ["blocking"]
+  );
